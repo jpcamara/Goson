6e420f0c299232df2c5f174a7fc1512b1c9773d9
JP Camara <johnpcamara@new-host.home>
2011-08-11 20:08:23 -0400

    reformatted

--- src/com/jpcamara/goson/JsonTypeLoader.java
+++ src/com/jpcamara/goson/JsonTypeLoader.java
@@ -22,25 +22,24 @@ import java.util.Set;
 import org.json.JSONObject;
 
 public class JsonTypeLoader extends TypeLoaderBase {
-	private Map<String, IType> types = new HashMap<String, IType>();
-	private static final String EXT = "json";
-	
-	public JsonTypeLoader(IModule env) {
-		super(env);
-	}
+  private Map<String, IType> types = new HashMap<String, IType>();
+  private static final String EXT = "json";
 
-	@Override
-	public IType getType(String fullyQualifiedName) {
+  public JsonTypeLoader(IModule env) {
+    super(env);
+  }
+
+  @Override
+  public IType getType(String fullyQualifiedName) {
     maybeInitTypes();
     if (fullyQualifiedName == null || types.get(fullyQualifiedName) == null) {
-			return null;
-		}
+      return null;
+    }
     IType iType = types.get( fullyQualifiedName );
     return TypeSystem.getOrCreateTypeReference( iType );
-	}
+  }
 
-  private void maybeInitTypes()
-  {
+  private void maybeInitTypes() {
     if (types.isEmpty()) {
       List<JsonFile> files = jsonFiles.get();
       for (JsonFile file : files) {
@@ -55,126 +54,119 @@ public class JsonTypeLoader extends TypeLoaderBase {
   }
 
   private void addType(String name, String path, JsonParser o) {
-		JsonName typeName = new JsonName(name);
-		JsonType type = new JsonType(typeName, path, this, o);
-		types.put(path + "." + typeName.getName(), type);
-	}
-	
-	private void addEnumType(String name, String path, JsonParser o) {
-		JsonName typeName = new JsonName(name);
-		JsonEnumType type = new JsonEnumType(typeName, path, this, o);
-		types.put(path + "." + typeName.getName(), type);
-	}
+    JsonName typeName = new JsonName(name);
+    JsonType type = new JsonType(typeName, path, this, o);
+    types.put(path + "." + typeName.getName(), type);
+  }
 
-	private void searchAndAddTypes(String name, String path, JsonParser object)
-			throws Exception {
-		for (String key : object.keys()) {
-			Object obj = object.get(key);
-			if (JsonParser.isJSONObject(obj)) {
-			  try {
-			    if (((JSONObject)obj).has("enum")) {
-			      addEnumType(key, path, object.getJsonParser(key));
-			    } else {
-			      searchAndAddTypes(key, path, object.getJsonParser(key));
-    				addType(key, path, object.getJsonParser(key));
-			    }
-			  } catch (Exception e) {
-			    e.printStackTrace();
-			    //This shouldn't throw an exception anyway, oy
-			  }
-			} else if (JsonParser.isJSONArray(obj)) {
-				Object arrEntry = object.getWithIndex(key, 0);
-				if (JsonParser.isJSONObject(arrEntry)) {
-					JsonParser typeInArray = new JsonParser(arrEntry);
-/*          searchAndAddTypes(key, path, typeInArray);
-          addType(key, path, typeInArray);*/
-					
-					
-					try {
-  			    if (typeInArray.get("enum") != null) {
-  			      addEnumType(key, path, typeInArray);
-  			    } else {
-  			      searchAndAddTypes(key, path, typeInArray);
-      				addType(key, path, typeInArray);
-  			    }
-  			  } catch (Exception e) {
-  			    e.printStackTrace();
-  			    //This shouldn't throw an exception anyway, oy
-  			  }
-					
-					
-					
-					
-				}
-			}
-		}
-	}
+  private void addEnumType(String name, String path, JsonParser o) {
+    JsonName typeName = new JsonName(name);
+    JsonEnumType type = new JsonEnumType(typeName, path, this, o);
+    types.put(path + "." + typeName.getName(), type);
+  }
 
-	@Override
-	public Set<String> getAllTypeNames() {
+  private void searchAndAddTypes(String name, String path, JsonParser object)
+    throws Exception {
+    for (String key : object.keys()) {
+      Object obj = object.get(key);
+      if (JsonParser.isJSONObject(obj)) {
+        try {
+          if (((JSONObject)obj).has("enum")) {
+            addEnumType(key, path, object.getJsonParser(key));
+          } else {
+            searchAndAddTypes(key, path, object.getJsonParser(key));
+            addType(key, path, object.getJsonParser(key));
+          }
+        } catch (Exception e) {
+          e.printStackTrace();
+          //This shouldn't throw an exception anyway, oy
+        }
+      } else if (JsonParser.isJSONArray(obj)) {
+        Object arrEntry = object.getWithIndex(key, 0);
+        if (JsonParser.isJSONObject(arrEntry)) {
+          JsonParser typeInArray = new JsonParser(arrEntry);
+
+          try {
+            if (typeInArray.get("enum") != null) {
+              addEnumType(key, path, typeInArray);
+            } else {
+              searchAndAddTypes(key, path, typeInArray);
+              addType(key, path, typeInArray);
+            }
+          } catch (Exception e) {
+            e.printStackTrace();
+            //This shouldn't throw an exception anyway, oy
+          }
+        }
+      }
+    }
+  }
+
+  @Override
+  public Set<String> getAllTypeNames() {
     maybeInitTypes();
     return new HashSet<String>( types.keySet() );
-	}
+  }
 
-	@Override
-	public List<String> getHandledPrefixes() {
-		List<String> prefixes = Arrays.asList(getAllNamespaces().toArray(new String[] {}));
-		return prefixes;
-	}
-	
-	/*
-	* Default implementation to handle Gosu 0.9 reqs
-	*/
+  @Override
+  public List<String> getHandledPrefixes() {
+    List<String> prefixes = Arrays.asList(getAllNamespaces().toArray(new String[] {}));
+    return prefixes;
+  }
+
+  /*
+  * Default implementation to handle Gosu 0.9 reqs
+  */
   @Override
   public boolean handlesNonPrefixLoads() {
     return true;
   }
-	
-	private LazyVar<List<JsonFile>> jsonFiles = new LazyVar<List<JsonFile>>() {
-		@Override
-		protected List<JsonFile> init() {
-			List<JsonFile> init = new java.util.ArrayList<JsonFile>();
-			
-			List<Pair<String, IFile>> files = 
-				JsonTypeLoader.this.getModule().getResourceAccess().findAllFilesByExtension(EXT);
-			for (Pair<String, IFile> pair : files) {
-				JsonFile current = new JsonFile();
-				
-				String fileName = pair.getSecond().getName().replaceAll("\\.json", "");
-				String path = pair.getFirst().replaceAll(pair.getSecond().getName(), "");
-				if (path.isEmpty()) {
-				  throw new RuntimeException("Cannot have Simple JSON Schema definitions in the default package");
-				}
-				
-				int lastSeparatorIndex = path.lastIndexOf(File.separator);
-			  path = path.substring(0, lastSeparatorIndex).replace(File.separator, ".");
-				
-				int lastIndex = fileName.lastIndexOf(".");
-				current.name = fileName.substring(lastIndex + 1);
-				current.path = path;
 
-				Scanner s = null;
-				try {
-					java.lang.StringBuilder jsonString = new java.lang.StringBuilder();
-					s = new Scanner(pair.getSecond().toJavaFile());
-					while (s.hasNextLine()) {
-						jsonString.append(s.nextLine());
-					}
-					current.content = new JsonParser(jsonString.toString());
-				} catch (FileNotFoundException e) {
-					throw new RuntimeException(e);
-				} finally {
-					if (s != null) { s.close(); }
-				}
-				init.add(current);
-			}
-			return init;
-		}
-	};
-	
-	private static class JsonFile {
-		private JsonParser content;
-		private String path;
-		private String name;
-	}
+  private LazyVar<List<JsonFile>> jsonFiles = new LazyVar<List<JsonFile>>() {
+    @Override
+    protected List<JsonFile> init() {
+      List<JsonFile> init = new java.util.ArrayList<JsonFile>();
+
+      List<Pair<String, IFile>> files = 
+      JsonTypeLoader.this.getModule().getResourceAccess().findAllFilesByExtension(EXT);
+      for (Pair<String, IFile> pair : files) {
+        JsonFile current = new JsonFile();
+
+        String fileName = pair.getSecond().getName().replaceAll("\\.json", "");
+        String path = pair.getFirst().replaceAll(pair.getSecond().getName(), "");
+        if (path.isEmpty()) {
+          throw new RuntimeException("Cannot have Simple JSON Schema definitions in the default package");
+        }
+
+        int lastSeparatorIndex = path.lastIndexOf(File.separator);
+        path = path.substring(0, lastSeparatorIndex).replace(File.separator, ".");
+
+        int lastIndex = fileName.lastIndexOf(".");
+        current.name = fileName.substring(lastIndex + 1);
+        current.path = path;
+
+        Scanner s = null;
+        try {
+          java.lang.StringBuilder jsonString = new java.lang.StringBuilder();
+          s = new Scanner(pair.getSecond().toJavaFile());
+          while (s.hasNextLine()) {
+            jsonString.append(s.nextLine());
+          }
+          current.content = new JsonParser(jsonString.toString());
+        } catch (FileNotFoundException e) {
+          throw new RuntimeException(e);
+        } finally {
+          if (s != null) { s.close(); }
+        }
+        init.add(current);
+      }
+      return init;
+    }
+  };
+
+  private static class JsonFile {
+    private JsonParser content;
+    private String path;
+    private String name;
+  }
 }
