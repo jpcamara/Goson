14354bd66257015750260a5127622571c0d33a73
Rick Kitts <rkitts@loudhouse.org>
2011-10-16 13:01:55 -0700

    - typedefs@ must now be followed by an struct def - multiple typedefs@ defs are merged - duplicate typedefs@ defs are an error

--- src/org/jschema/parser/JSchemaParser.java
+++ src/org/jschema/parser/JSchemaParser.java
@@ -2,8 +2,10 @@ package org.jschema.parser;
 
 import org.jschema.model.JsonMap;
 import org.jschema.util.JSchemaUtils;
+import sun.java2d.SunGraphicsEnvironment;
 
 import java.util.Map;
+import java.util.Set;
 
 public class JSchemaParser extends JSONParser {
 
@@ -20,27 +22,67 @@ public class JSchemaParser extends JSONParser {
   @Override
   protected void putWithSemantics(JsonMap map, String key, Object value)
   {
-    JsonParseError error = null;
     if(JSchemaUtils.JSCHEMA_TYPEDEFS_KEY.equals(key)){
-      if(value instanceof Map == false){
-        // Real tight coupling. We've consumed the typedefs@ and the : and we're sitting on whatever is after that.
-        JSONToken errorToken = _currentToken;
-        int errorLine = -1;
-        int errorCol = -1;
-        while(errorToken != null && errorToken.match("typedefs@") == false){
-          errorToken = errorToken.previous();
-        }
-        if(errorToken != null){
-          errorLine = errorToken.getLine();
-          errorCol = errorToken.getColumn();
-        }
-
-        error = new JsonParseError("typedefs@ at line " + errorLine + " column " + errorCol + " is not followed by an struct definition");
-        _errors.add(error);
-      }
+      processTypedefs(map, key, value);
     }
-    if(error == null){
+    else{
       super.putWithSemantics(map, key, value);
     }
+  }
+
+  private void processTypedefs(JsonMap map, String key, Object value)
+  {
+    JsonParseError error = null;
+    if(value instanceof Map == false){
+      // Real tight coupling. We've consumed the typedefs@ and the : and we're sitting on whatever is after that.
+      JSONToken errorToken = _currentToken;
+      int errorLine = -1;
+      int errorCol = -1;
+      while(errorToken != null && errorToken.match("typedefs@") == false){
+        errorToken = errorToken.previous();
+      }
+      if(errorToken != null){
+        errorLine = errorToken.getLine();
+        errorCol = errorToken.getColumn();
+      }
+
+      error = new JsonParseError("typedefs@ at line " + errorLine + " column " + errorCol + " is not followed by an struct definition");
+      _errors.add(error);
+    }
+
+    if(error == null){
+      // Handle multiple typedefs@ in at the same level in a single document
+      JsonMap previousTypedefs = (JsonMap) map.get(JSchemaUtils.JSCHEMA_TYPEDEFS_KEY);
+      if(previousTypedefs != null){
+        // Merge the typedefs
+        JsonMap newTypedefs = (JsonMap) value;
+        Set<? extends String> keys = newTypedefs.keySet();
+        for(String newTypeName : keys){
+          if(previousTypedefs.containsKey(newTypeName) == true){
+            // TODO: Centralize this error handling
+            JSONToken errorToken = _currentToken;
+            int errorLine = -1;
+            int errorCol = -1;
+            while(errorToken != null && errorToken.match("typedefs@") == false){
+              errorToken = errorToken.previous();
+            }
+            if(errorToken != null){
+              errorLine = errorToken.getLine();
+              errorCol = errorToken.getColumn();
+            }
+
+            error = new JsonParseError("duplicate type " + newTypeName + " declared at line " + errorLine + " column " + errorCol);
+            _errors.add(error);
+          }
+          else{
+            previousTypedefs.putAll(newTypedefs);
+          }
+        }
+      }
+      else{
+        map.put(key, value);
+      }
+    }
+    return;
   }
 }
--- test/org/jschema/parser/JSchemaParserTest.java
+++ test/org/jschema/parser/JSchemaParserTest.java
@@ -1,6 +1,12 @@
 package org.jschema.parser;
 
+import com.sun.xml.internal.bind.v2.model.core.TypeInfo;
+import org.eclipse.jetty.util.ajax.JSON;
+import org.jschema.model.JsonMap;
 import org.jschema.test.GosonTest;
+import org.jschema.util.JSchemaUtils;
+
+import java.util.Map;
 
 public class JSchemaParserTest extends GosonTest {
 
@@ -16,8 +22,61 @@ public class JSchemaParserTest extends GosonTest {
       // gulp
     }
 
-    String goodJSchema = "{ \"typedefs@\" : { \"name\" : \"string\"}}";
+    String goodJSchema = "{ \"typedefs@\" : {\n" +
+            "    \"MyType\" : {\n" +
+            "       \"line1\" : \"string\"\n" +
+            "     }\n" +
+            "   }\n" +
+            "}";
     parser = new JSchemaParser(goodJSchema);
     parser.parseJSchema();
   }
+
+  public void testMutltipleTypedefsAreMerged()
+  {
+    String schema = "{ \n" +
+            "\n" +
+            "\"typedefs@\" : {\n" +
+            "    \"MyNewType\" : {\n" +
+            "       \"line1\" : \"string\"\n" +
+            "    }\n" +
+            " },   \n" +
+            " \"typedefs@\" : {\n" +
+            "    \"MyOtherType\" : {\n" +
+            "       \"line2\" : \"string\"\n" +
+            "     }\n" +
+            "   }   \n" +
+            "}";
+    JSchemaParser parser = new JSchemaParser(schema);
+    JsonMap schemaMap = (JsonMap) parser.parseJSchema();
+    JsonMap typedefsMap = (JsonMap) schemaMap.get(JSchemaUtils.JSCHEMA_TYPEDEFS_KEY);
+    assertEquals(2, typedefsMap.size());
+  }
+
+  public void testDuplicateTypesdefsAreErrors()
+  {
+    String schema = "{ \n" +
+            "\n" +
+            "\"typedefs@\" : {\n" +
+            "    \"MyNewType\" : {\n" +
+            "       \"line1\" : \"string\"\n" +
+            "    }\n" +
+            " },   \n" +
+            " \"typedefs@\" : {\n" +
+            "    \"MyNewType\" : {\n" +
+            "       \"line2\" : \"string\"\n" +
+            "     }\n" +
+            "   }   \n" +
+            "}";
+    JSchemaParser parser = new JSchemaParser(schema);
+    try{
+      parser.parseJSchema();
+      fail("Exception not thrown");
+    }
+    catch(JsonParseException jpe){
+      System.out.println(jpe.toString());
+      // Gulp
+    }
+  }
+
 }
